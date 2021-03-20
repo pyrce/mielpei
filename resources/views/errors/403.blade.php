4254dbebc5bd2dbff38472625fe614e32df44f58
florent etheve <fetheve2@gmail.com>
2021-03-20 11:28:49 +0400

    errors blade

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,16 @@
+@extends("layout")
+
+@section("content")
+
+<div class="container border rows">
+
+<h1 class="bg-danger row justify-content-center">Erreur</h1>
+
+<h4  class="row justify-content-center error-code my-5 ">403</h4>
+
+<p class="row justify-content-center my-5 error-msg">Vous n'êtes pas autorisé à accéder à cette page</p>
+
+
+
+</div>
+@endsection
